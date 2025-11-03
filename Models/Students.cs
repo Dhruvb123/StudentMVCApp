c18041c5eac48fc40e8628739e6cf2377dee2dc3
Dhruv Bheda <dhruvbheda009@gmail.com>
2025-11-03 12:32:55 +0530

    first commit

---
+++ Models/Students.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCCourse.Models
+{
+    public class Students
+    {
+        public int StudentId { get; set; }
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public decimal Result { get; set; }
+    }
+}
